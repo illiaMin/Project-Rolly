7fbb2441f322e291ce535419558c5040c69211c9
Illia Minhazau <minhazauillia@gmail.com>
2026-01-30 13:41:57 +0100

    feat: add modules database scripts

---
+++ Unity/Assets/Scripts/Runtime/DB AllModules/SO_AllModules.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO_AllModules", menuName = "Scriptable Objects/SO_AllModules")]
+public class SO_AllModules : ScriptableObject
+{
+    public SerializableStringModuleDictionary Modules;
+}
---
+++ Unity/Assets/Scripts/Runtime/DB AllModules/SerializableStringModuleDictionary.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SerializableStringModuleDictionary
+{
+    [Serializable]
+    private struct Entry
+    {
+        public ModuleName Key;
+        public ScriptableObject Value;
+    }
+
+    [SerializeField]
+    private List<Entry> _entries = new List<Entry>();
+
+    private Dictionary<ModuleName, ScriptableObject> _dictionary;
+
+    public bool TryGet(ModuleName key, out ScriptableObject value)
+    {
+        EnsureBuilt();
+        return _dictionary.TryGetValue(key, out value);
+    }
+
+    public ScriptableObject Get(ModuleName key)
+    {
+        EnsureBuilt();
+
+        if (!_dictionary.TryGetValue(key, out ScriptableObject value))
+        {
+            throw new KeyNotFoundException($"Key '{key}' not found.");
+        }
+
+        return value;
+    }
+
+    public bool ContainsKey(ModuleName key)
+    {
+        EnsureBuilt();
+        return _dictionary.ContainsKey(key);
+    }
+
+    public void Rebuild()
+    {
+        _dictionary = new Dictionary<ModuleName, ScriptableObject>();
+
+        for (int i = 0; i < _entries.Count; i++)
+        {
+            Entry entry = _entries[i];
+
+            if (_dictionary.ContainsKey(entry.Key))
+            {
+                Debug.LogWarning($"Duplicate key '{entry.Key}'");
+                continue;
+            }
+
+            _dictionary.Add(entry.Key, entry.Value);
+        }
+    }
+
+    private void EnsureBuilt()
+    {
+        if (_dictionary == null)
+        {
+            Rebuild();
+        }
+    }
+}
