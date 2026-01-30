290a47aa301a045e9cd49ca4506a912d622052d4
Illia Minhazau <minhazauillia@gmail.com>
2026-01-30 13:36:44 +0100

    refactor: remove legacy turret database system

--- Unity/Assets/Scripts/Runtime/Combat/Data/SO_AllTurrets.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SO_AllTurrets", menuName = "Scriptable Objects/SO_AllTurrets")]
-public class SO_AllTurrets : ScriptableObject
-{
-    public SerializableStringTurretDictionary Turrets;
-}
